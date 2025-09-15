8d999416d63ca2fba059f75b08c115108effdbc4
raydatorre <lykewer@gmail.com>
2025-09-15 17:30:28 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+// app/layout.tsx
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="pt-BR">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
